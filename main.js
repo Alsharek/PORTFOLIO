ab3229fcaadde75d8c6d3298928045e3caeee94a
Ammar Al-Sharek <91220825+Alsharek@users.noreply.github.com>
2022-03-09 14:47:45 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import ./style.css'
+require ('./style.css')
 import * as THREE from 'three';
 //import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 //import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.121.1/build/three.module.js';
